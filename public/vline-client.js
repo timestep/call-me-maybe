3d29839c690fa7e8f55bc73bcc9b95b4a246b571
timestep <jbosshellokitty@gmail.com>
2013-07-02 14:47:36 -0400

    code refacotring

--- public/vline-client.js
+++ public/vline-client.js
@@ -1,21 +1,37 @@
 function vlineShell(serviceId) {
-
 	this.client_ = vline.Client.create({'serviceId': serviceId});
 	
 	this.client_.login(serviceId, window.PROFILE, window.AUTH_TOKEN)
-			.done(function(session) {
-        this.session_ = session;
-        remoteUserId = this.session_.mb;
-        console.log(remoteUserId);
+		.done(function(session) {
+      this.session_ = session;
+      console.log(remoteUserId);
+      vline.client_.on('recv:im', this.onMessage_, this);
         
-      ,this);
-  });
+    }, this);
 
+	// this.client_.on('add:mediaSession', onMediaSession, this);
+
+ //  function onMediaSession(event) {
+ //    var mediaSession = event.target;
+ //    if (mediaSession.isIncoming()) {
+ //      this.calls_.push(new MyCall(this, mediaSession));
+ //    }
+ //  };
+
+ //  person.startMedia();
+	
 };
 
-function connection(remoteUserId){
-  this.session_.getPerson(remoteUserId).done(function(person) {
-    console.log('YOLO');
-    person.postMessage("Hello there");
-  };
+vlineShell.prototype.onMessage_ = function(event) {
+  var msg = event.message,
+      sender = msg.getSender();
+  this.showAlert(sender.getDisplayName(),
+                 sender.getThumbnailUrl(),
+                 msg.getBody());
+};
+
+vlineShell.prototype.sendMessage_= function(userId){
+  session_ .getPerson(userId).done(function(person){
+    person.postMessage("Yo");
+  })
 };
