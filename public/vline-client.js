b8a11306e33d20df9def3a2ba2c2bde455ea18fb
timestep <jbosshellokitty@gmail.com>
2013-07-02 15:41:53 -0400

    added calling functions

--- public/vline-client.js
+++ public/vline-client.js
@@ -5,7 +5,14 @@ function vlineShell(serviceId) {
 		.done(function(session) {
       this.session_ = session;
       console.log(vline.session_.mb);
-      vline.client_.on('recv:im', this.onMessage_, this);
+      vline.client_.on('add:mediaSession', onMediaSession, this)
+
+      function onMediaSession(event) {
+        var mediaSession = event.target;
+        if (mediaSession.isIncoming()) {
+          this.calls_.push(new MyCall(this, mediaSession));
+        }
+      }
         
     }, this);
 
@@ -19,8 +26,58 @@ function vlineShell(serviceId) {
  //  };
 
  //  person.startMedia();
-	
 };
+
+
+vlineShell.prototype.call = function(userId) {
+ this.session_.getPerson(userId).
+    done(function(person) {
+      this.calls_.push(new MyCall(this, person.startMedia());
+      person.release();
+    }, this);
+};
+
+// constructor of two-party call controller
+function MyCall(app, mediaSession) {
+
+  mediaSession.
+    on('enterState:pending', onEnterPending).
+    on('exitState:pending', app.hideModal, app).
+    on('enterState:incoming', onEnterIncoming).
+    on('exitState:incoming', app.hideNotification, app).
+    on('enterState:outgoing', onEnterOutgoing).
+    on('exitState:outgoing', app.hideMessage, app).
+    on('enterState:connecting', onEnterConnecting).
+    on('exitState:connecting', app.hideMessage, app).
+    on('enterState:active', onEnterActive).
+    on('exitState:active', app.hideCallUi, app);
+
+  function onEnterPending() {
+    app.showModal("Click 'allow' to start call ^^^");
+  }
+  function onEnterIncoming() {
+    app.showNotification(
+      'Incoming call from ' + mediaSession.getDisplayName() + '...',
+      mediaSession.getThumbnailUrl());
+  }
+  function onEnterOutgoing() {
+    app.showMessage(
+     'Calling ' + mediaSession.getDisplayName() + '...',
+      mediaSession.getThumbnailUrl());
+  }
+  function onEnterConnecting() {
+    app.showMessage('Connecting...');
+  }
+  function onEnterActive() {
+    app.showCallUi(mediaSession);
+  }
+}
+
+// end the call
+MyCall.prototype.end = function() {
+  this.mediaSession_.stop();
+};
+
 
 vlineShell.prototype.onMessage_ = function(event) {
   var msg = event.message,
