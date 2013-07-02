293d4779488cb9c98f8f6a47c0f6bdf1f3521284
timestep <jbosshellokitty@gmail.com>
2013-07-02 14:05:13 -0400

    adding connection method

--- public/vline-client.js
+++ public/vline-client.js
@@ -3,17 +3,19 @@ function vlineShell(serviceId) {
 	this.client_ = vline.Client.create({'serviceId': serviceId});
 	
 	this.client_.login(serviceId, window.PROFILE, window.AUTH_TOKEN)
-			.done(function(session) {this.session_ = session;}, this);
+			.done(function(session) {
+        this.session_ = session;
+        remoteUserId = this.session_.mb;
+        console.log(remoteUserId);
+        
+      ,this);
+  });
 
-	this.client_.on('add:mediaSession', onMediaSession, this);
+};
 
-  function onMediaSession(event) {
-    var mediaSession = event.target;
-    if (mediaSession.isIncoming()) {
-      this.calls_.push(new MyCall(this, mediaSession));
-    }
+function connection(remoteUserId){
+  this.session_.getPerson(remoteUserId).done(function(person) {
+    console.log('YOLO');
+    person.postMessage("Hello there");
   };
-
-
-	
 };
