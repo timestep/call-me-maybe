b0564d7ed36588bad51c48afc5fd54c8d1006d2b
timestep <jbosshellokitty@gmail.com>
2013-07-02 17:58:27 -0400

    code

--- public/vline-client.js
+++ public/vline-client.js
@@ -5,68 +5,69 @@ function vlineShell(serviceId) {
 	this.client_.login(serviceId, window.PROFILE, window.AUTH_TOKEN)
 		.done(function(session) {
       this.session_ = session;
-      console.log(vlineState.session_.mb);
-      vlineState.client_.on('add:mediaSession', onMediaSession, this)
-      vlineState.client_.on('recv:im', this.onMessage_, this);
+      console.log(this.session_.mb);
 
-      function onMediaSession(event) {
-        var mediaSession = event.target;
-        if (mediaSession.isIncoming()) {
-          this.calls_.push(new MyCall(this, mediaSession));
-        }
-      }
+      this.client_.on('add:mediaSession', onMediaSession, this)
+      this.client_.on('recv:im', this.onMessage_, this);
+
+      
     }, this);
 };
 
-
-// vlineShell.prototype.call = function(userId) {
-//  this.session_.getPerson(userId).
-//     done(function(person) {
-//       this.calls_.push(new MyCall(this, person.startMedia());
-//       // person.release();
-//     }, this);
-  // constructor of two-party call controller
-  function MyCall(app, mediaSession) {
-
-    mediaSession.
-      on('enterState:pending', onEnterPending).
-      on('exitState:pending', app.hideModal, app).
-      on('enterState:incoming', onEnterIncoming).
-      on('exitState:incoming', app.hideNotification, app).
-      on('enterState:outgoing', onEnterOutgoing).
-      on('exitState:outgoing', app.hideMessage, app).
-      on('enterState:connecting', onEnterConnecting).
-      on('exitState:connecting', app.hideMessage, app).
-      on('enterState:active', onEnterActive).
-      on('exitState:active', app.hideCallUi, app);
-
-    function onEnterPending() {
-      app.showModal("Click 'allow' to start call ^^^");
-    }
-    function onEnterIncoming() {
-      app.showNotification(
-        'Incoming call from ' + mediaSession.getDisplayName() + '...',
-        mediaSession.getThumbnailUrl());
-    }
-    function onEnterOutgoing() {
-      app.showMessage(
-       'Calling ' + mediaSession.getDisplayName() + '...',
-        mediaSession.getThumbnailUrl());
-    }
-    function onEnterConnecting() {
-      app.showMessage('Connecting...');
-    }
-    function onEnterActive() {
-      app.showCallUi(mediaSession);
-    }
-  }
-
-  // end the call  
-  MyCall.prototype.end = function() {
-    this.mediaSession_.stop();
+vlineShell.prototype.onMediaSession = function(event){
+  var mediaSession = event.target;
+    if (mediaSession.isIncoming()) {
+      this.calls_.push(new MyCall(this, mediaSession));
   };
-// };
+};
 
+vlineShell.prototype.call = function(userId) {
+  this.session_.getPerson(userId).done(function(person) {
+      this.calls_.push(new MyCall(this, person.startMedia()));
+      person.release();
+    }, this);
+  // constructor of two-party call controller
+};
+
+function MyCall(app, mediaSession) {
+
+  mediaSession.
+    on('enterState:pending', onEnterPending).
+    on('exitState:pending', app.hideModal, app).
+    on('enterState:incoming', onEnterIncoming).
+    on('exitState:incoming', app.hideNotification, app).
+    on('enterState:outgoing', onEnterOutgoing).
+    on('exitState:outgoing', app.hideMessage, app).
+    on('enterState:connecting', onEnterConnecting).
+    on('exitState:connecting', app.hideMessage, app).
+    on('enterState:active', onEnterActive).
+    on('exitState:active', app.hideCallUi, app);
+
+  function onEnterPending() {
+    console.log("Click 'allow' to start call ^^^");
+  }
+  function onEnterIncoming() {
+    console.log(
+      'Incoming call from ' + mediaSession.getDisplayName() + '...',
+      mediaSession.getThumbnailUrl());
+  }
+  function onEnterOutgoing() {
+    console.log(
+     'Calling ' + mediaSession.getDisplayName() + '...',
+      mediaSession.getThumbnailUrl());
+  }
+  function onEnterConnecting() {
+    console.log('Connecting...');
+  }
+  function onEnterActive() {
+    app.showCallUi(mediaSession);
+  }
+};
+
+// end the call  
+MyCall.prototype.end = function() {
+  this.mediaSession_.stop();
+};
 
 
 
