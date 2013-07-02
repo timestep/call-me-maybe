833c2214786b71b0d0d33a32e22183c1a47f7f96
timestep <jbosshellokitty@gmail.com>
2013-07-02 13:33:31 -0400

    added sessions

--- public/vline-client.js
+++ public/vline-client.js
@@ -3,20 +3,16 @@ function vlineShell(serviceId) {
 	this.client_ = vline.Client.create({'serviceId': serviceId});
 	
 	this.client_.login(serviceId, window.PROFILE, window.AUTH_TOKEN)
-			.done(this.init_, this);
+			.done(function(session) {this.session_ = session;}, this);
 
-	// function vlineShell.prototype.init_ = function(session) {
-	// this.session_ = session;
-  //  	};		
-
-  this.client_.on('add:mediaSession', onMediaSession, this);
+	this.client_.on('add:mediaSession', onMediaSession, this);
 
   function onMediaSession(event) {
     var mediaSession = event.target;
     if (mediaSession.isIncoming()) {
       this.calls_.push(new MyCall(this, mediaSession));
     }
-  }
+  };
 
 
 	
