2607a77944c069e1c8a06c6a431cd26c0e85b46a
timestep <jbosshellokitty@gmail.com>
2013-07-03 09:34:50 -0400

    works flawlessl; better than shitty other version

--- public/vline-client.js
+++ public/vline-client.js
@@ -3,13 +3,17 @@ function vlineShell(serviceId) {
 
   this.calls_ = [];
 
-	this.client_ = vline.Client.create({'serviceId': serviceId});
+	this.client_ = vline.Client.create({
+    'serviceId': serviceId,
+    ui:true,
+    uiVideoPanel:'#video-wrapper'
+  });
 	this.client_.login(serviceId, window.PROFILE, window.AUTH_TOKEN)
 		.done(function(session) {
       this.session_ = session;
       console.log(this.session_.mb);
 
-      this.client_.on('add:mediaSession', this.onMediaSession, this)
+      // this.client_.on('add:mediaSession', this.onMediaSession, this)
       // this.client_.on('recv:im', this.onMessage_, this);
 
       
